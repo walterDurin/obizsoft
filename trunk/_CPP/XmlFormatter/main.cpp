e66759cf7ed6362720ff20d071702cdb6de67d5e
ognivo777@gmail.com <ognivo777@gmail.com@8fdd1a02-da89-11de-849f-b362a63f070a>
2012-12-01 00:27:11 +0000

    Make it works :)

--- trunk/_CPP/XmlFormatter/main.cpp
+++ trunk/_CPP/XmlFormatter/main.cpp
@@ -9,53 +9,25 @@ using namespace std;
 int main()
 {
 
-    /*
-    const DWORD BUFFER_SIZE = 2000;
-    char buffer[ BUFFER_SIZE ] ;
-
-    HANDLE hPipeRead, hPipeWrite;
-    CreatePipe( &hPipeRead, &hPipeWrite, 0, 0 );
-    HANDLE hConOut = GetStdHandle( STD_OUTPUT_HANDLE );
-    SetStdHandle( STD_OUTPUT_HANDLE, hPipeWrite );
-
-    STARTUPINFO startInfo = { sizeof(STARTUPINFO) };
-    PROCESS_INFORMATION procInfo;
-    //CreateProcess( "c:/windows/system32/ipconfig.exe", "", 0, 0, false, 0, 0, 0, &startInfo, &procInfo );
-
-    //SetStdHandle( STD_OUTPUT_HANDLE, hConOut );
-
-    DWORD nRead = 0;
-    ReadFile( hPipeRead, buffer, BUFFER_SIZE - 1, &nRead, 0 );
-    buffer[ nRead ] = 0;
-
-    cout << XmlUtils::formatXml(string(buffer));
-    // std::cout << "\n=====\n" << buffer << "\n=====\n";
-    */
-
-
-
-/*
-    string s;
-    char ch;
-    while(ch!='q') {
-        cin.get(ch);
-        //cin>>s;
-        cout<<ch;
+    std::stringstream sb;
+    char c ;
+    while(true) {
+        c = cin.get();
+        if(c<0) {
+            break;
+        }
+        sb << c;
     }
-*/
 
+    string xml = sb.str();
+    cout << XmlUtils::formatXml(xml);
+    return 0;
 /*
-    stringstream ss ;
-    ss << "aaa";
-    cout<<"|"<< ss.str() << "|" << XmlUtils::XmlFormatter::buildWhitespace(5)<<"|";
-    string a = "asdasdaasddffg";
-    cout << StringTools::Replace(a, string("a"), string("W")) <<"\n";
-*/
-
-    string xml("<a><b></b><c><d></d></c></a>");
+    string xml = "<a><b>as<c>as</c></b><d/></a>";
+    //string xml = "<a>bbb</a>";
     cout << "Xml: \n" << xml <<"\n";
     cout << XmlUtils::formatXml(xml);
-
     return 0;
+*/
 }
 
