a69ec882731bd97f0d79a10d74d8a047974e9533
ognivo777@gmail.com <ognivo777@gmail.com@8fdd1a02-da89-11de-849f-b362a63f070a>
2012-12-12 21:50:10 +0000

    Added help window

---
+++ trunk/ConturMonitor/src/ru/lanit/dibr/utils/utils/Utils.java
@@ -0,0 +1,15 @@
+package ru.lanit.dibr.utils.utils;
+
+import java.io.InputStream;
+
+/**
+ * User: Vova
+ * Date: 13.12.12
+ * Time: 1:18
+ */
+public class Utils {
+
+    public static String inputStreamToString(InputStream is){
+        return "";
+    }
+}
