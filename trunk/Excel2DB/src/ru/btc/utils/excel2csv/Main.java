96231179f84c16684782b7c2d4a9c153c788e0d3
ognivo777@gmail.com <ognivo777@gmail.com@8fdd1a02-da89-11de-849f-b362a63f070a>
2014-01-29 15:13:27 +0000

    stream processing

--- trunk/Excel2DB/src/ru/btc/utils/excel2csv/Main.java
+++ trunk/Excel2DB/src/ru/btc/utils/excel2csv/Main.java
@@ -4,105 +4,111 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.eventusermodel.XLSXReader;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.BatchUpdateException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by IntelliJ IDEA.
  * User: Vova
  * Date: 28.01.14
  * Time: 0:48
  */
 public class Main {
 
+    private static DataBaseWorker dataBaseWorker;
+
+
     public static void main(String[] args) throws IOException, SQLException {
-        String fileName = args[0];
-        String sheetName = args[1];
-        String skipFistLine = args[2];
-        String dbTableName = args[3];
-        String dbValuesFrom = args[4];
-        String dbURI = args[5];
-        String dbUser = args[6];
-        String dbPass = args[7];
+        int argNum = 0;
+        String fileName = args[argNum++];
+        String sheetName = args[argNum++];
+        String skipFistLine = args[argNum++];
+        String dbTableName = args[argNum++];
+        String dbFields = args[argNum++];
+        String dbValuesFrom = args[argNum++];
+        String dbURI = args[argNum++];
+        String dbUser = args[argNum++];
+        String dbPass = args[argNum++];
 
         System.out.println("Параметры запуска:");
         for (String arg : args) {
             System.out.print("\"" + arg + "\" ");
         }
 
-//        Connection connection = getConnection(dbURI, dbUser, dbPass);
-//        System.out.println("\nConnected: " + connection.getMetaData().getDatabaseProductName() + " ver. " +connection.getMetaData().getDatabaseProductVersion());
+        int rowsToProcess = 100000;
+        dataBaseWorker = new DataBaseWorker(dbURI, dbUser, dbPass, 1000);
 
-        System.out.println("\nОткрытие файла: \"" + fileName + "\"..");
+        try {
 
-        InputStream in = new FileInputStream(fileName);
-        // Внимание InputStream будет закрыт
-        // Если нужно не закрывающий см. JavaDoc по POIFSFileSystem :  http://goo.gl/1Auu7
-        XSSFWorkbook wb = new XSSFWorkbook(in);
+            System.out.println(new Date());
+            System.out.println("\nОткрытие файла: \"" + fileName + "\"..");
 
-//        Sheet sheet = wb.getSheetAt(cc[0]);
-        System.out.println("Открытие листа: \"" + sheetName + "\"..");
+            InputStream in = new FileInputStream(fileName);
+            // Внимание InputStream будет закрыт
+            // Если нужно не закрывающий см. JavaDoc по POIFSFileSystem :  http://goo.gl/1Auu7
+            XSSFWorkbook wb = new XSSFWorkbook(in);
+
+            System.out.println("Открытие листа: \"" + sheetName + "\"..");
 
 //        AreaReference aref = new AreaReference(sheetName + "!A3:E5");
 //        System.out.println(aref.getFirstCell().getRow() + "x" + aref.getFirstCell().getCol() + " - " + aref.getLastCell().getRow() + "x" + aref.getLastCell().getCol());
 
-        Sheet sheet = wb.getSheet(sheetName);
+            Sheet sheet = wb.getSheet(sheetName);
 
-        Iterator<Row> it = sheet.iterator();
-        if (it.hasNext() && skipFistLine != null && (skipFistLine.equalsIgnoreCase("Y") || skipFistLine.equalsIgnoreCase("Yes"))) {
-            System.out.println("Skip first line...");
-            it.next();
-        }
-
-        Pattern pattern = Pattern.compile("#(\\w+)");
-
-        while (it.hasNext()) {
-            Row row = it.next();
-
-            Iterator<Cell> cells = row.iterator();
-/*
-            List<String> lineVals = new ArrayList<String>();
-            while (cells.hasNext()) {
-                Cell cell = cells.next();
-                String val = getCellVal(cell);
-                lineVals.add(val);
+            Iterator<Row> it = sheet.iterator();
+            if (it.hasNext() && skipFistLine != null && (skipFistLine.equalsIgnoreCase("Y") || skipFistLine.equalsIgnoreCase("Yes"))) {
+                System.out.println("Skip first line...");
+                it.next();
             }
-*/
 
-            String sqlValues = dbValuesFrom;
-            Matcher m = pattern.matcher(dbValuesFrom);
-            while (m.find()) {
-                String ref = m.group(1);
-                Cell cell = row.getCell(new CellReference(ref).getCol());
-                String s;
-                if (cell == null) {
-                    s = "";
-                } else {
-                    s = getCellVal(cell);
+            Pattern pattern = Pattern.compile("#(\\w+)");
+
+            while (rowsToProcess-- > 0 && it.hasNext()) {
+                Row row = it.next();
+
+
+                String sqlValues = dbValuesFrom;
+                Matcher m = pattern.matcher(dbValuesFrom);
+                while (m.find()) {
+                    String ref = m.group(1);
+                    Cell cell = row.getCell(new CellReference(ref).getCol());
+                    String s;
+                    if (cell == null) {
+                        s = "";
+                    } else {
+                        s = getCellVal(cell);
+                    }
+                    s = s.replaceAll("'", "''");
+                    sqlValues = sqlValues.replaceFirst(m.group(), s);
                 }
-                s = s.replaceAll("'", "''");
-                sqlValues = sqlValues.replaceFirst(m.group(), s);
+
+                //sqlValues = sqlValues.substring(0, sqlValues.length() - 1);
+
+                String sql = "INSERT INTO " + dbTableName + "(" + dbFields + ") VALUES(" + sqlValues + ")";
+                //System.out.println(sqlValues);
+                dataBaseWorker.execSqlInBatch(sql);
             }
-
-            sqlValues = sqlValues.substring(0, sqlValues.length() - 1);
-
-            String sql = "INSERT INTO " + dbTableName + " VALUES(" + sqlValues + ")";
-
-            System.out.println(sql);
-
-            //connection.createStatement().execute(sql);
-
+            dataBaseWorker.commitAllSkipErrors();
+            //System.out.println(new Date());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            dataBaseWorker.close();
         }
+
 
     }
 
@@ -126,13 +132,4 @@ public class Main {
         return val;
     }
 
-
-    private static Connection getConnection(String uri, String user, String pass) throws SQLException {
-        Connection conn = null;
-        Properties connectionProps = new Properties();
-        connectionProps.put("user", user);
-        connectionProps.put("password", pass);
-        conn = DriverManager.getConnection(uri, connectionProps);
-        return conn;
-    }
 }
--- trunk/Excel2DB/src/ru/btc/utils/excel2csv/Test.java
+++ trunk/Excel2DB/src/ru/btc/utils/excel2csv/Test.java
@@ -1,5 +1,6 @@
 package ru.btc.utils.excel2csv;
 
+import java.sql.SQLException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,13 +13,15 @@ import java.util.regex.Pattern;
 
 
 public class Test {
-    public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("#(\\w+)");
-        String str = "#A,#B,#E";
-        Matcher m = pattern.matcher(str);
-        while (m.find()) {
-            System.out.println(m.group(1));
-            System.out.println(m.start() +  ":" + m.end());
-        }
+    public static void main(String[] args) throws SQLException {
+        DataBaseWorker dataBaseWorker = new DataBaseWorker("jdbc:oracle:thin:@127.0.0.1:2621:pega", "aptp", "prpc2184", 6);
+        dataBaseWorker.execSqlInBatch("insert into tst(val) values ('A')");
+        dataBaseWorker.execSqlInBatch("insert into tst(val) values ('A')");
+        dataBaseWorker.execSqlInBatch("insert into tst(val) values ('A')");
+        dataBaseWorker.execSqlInBatch("insert into tst(val) values ('B')");
+        dataBaseWorker.execSqlInBatch("insert into tst(val) values ('C')");
+        dataBaseWorker.execSqlInBatch("insert into tst(val) values ('D')");
+        dataBaseWorker.commitAllSkipErrors();
+        dataBaseWorker.close();
     }
 }
