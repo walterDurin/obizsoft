aab9c6dc90adbf57553010d0ac7fcc846b67f853
ognivo777@gmail.com <ognivo777@gmail.com@8fdd1a02-da89-11de-849f-b362a63f070a>
2014-01-31 14:30:38 +0000

    Optimized statement usage

---
+++ trunk/Excel2DB/src/ru/btc/utils/excel2csv/DataBaseWorker.java
@@ -0,0 +1,190 @@
+package ru.btc.utils.excel2csv;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * User: Vladimir
+ * Date: 28.01.14
+ * Time: 17:00
+ */
+public class DataBaseWorker {
+    private static DataBaseWorker worker = null;
+
+    private static Log log = LogFactory.getLog(DataBaseWorker.class);
+
+    private String uri = null;
+    private String user = null;
+    private String pass = null;
+    private int batchSize = 50;
+    private DataSource ds = null;
+    private Connection conn = null;
+    private Statement stmt = null;
+    private int unsavedRecords = 0;
+    private boolean isDirty = false;
+    private boolean initialised = false;
+    private boolean isClosed = false;
+    private double totalCommited = 0;
+    private double totalErrorsCount = 0;
+    private double executeCount = 0;
+
+    private List<String> sqls = new LinkedList<String>();
+
+
+    public DataBaseWorker(String uri, String user, String pass, int batchSize) {
+        if (worker != null) {
+            throw new RuntimeException("DBConnection already initialised! Please make sure that no parallel processes.");
+        }
+        this.uri = uri;
+        this.user = user;
+        this.pass = pass;
+        this.batchSize = batchSize;
+    }
+
+    private void init() throws RuntimeException {
+        try {
+            if (conn != null || initialised == true) {
+                throw new RuntimeException("DataBaseWorker already initialised!");
+            }
+            Properties connectionProps = new Properties();
+            connectionProps.put("user", user);
+            connectionProps.put("password", pass);
+            conn = DriverManager.getConnection(uri, connectionProps);
+            conn.setAutoCommit(false);
+            stmt = conn.createStatement();
+            initialised = true;
+        } catch (Exception e) {
+            throw new RuntimeException("Can't retrieve DataSource!", e);
+        }
+    }
+
+    public void execSqlInBatch(String sql) throws SQLException {
+        //if(true) return;
+        if (isClosed) {
+            log.error("Trying to use closed DataTableWorker!");
+            throw new RuntimeException("Trying to use closed DataTableWorker!");
+        }
+        if (!initialised) {
+            init();
+        }
+        sqls.add(sql);
+        stmt.addBatch(sql);
+        isDirty = true;
+        unsavedRecords++;
+        if (unsavedRecords == batchSize) {
+            //log.info("Save next " + unsavedRecords + " records.");
+            commit();
+        }
+    }
+
+    public void commitAllSkipErrors() {
+        while (unsavedRecords > 0) {
+            try {
+                commit();
+            } catch (SQLException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+    }
+
+    public void commit() throws SQLException {
+        if (isClosed) {
+            throw new RuntimeException("Trying to commit closed DataTableWorker!");
+        }
+        if (!initialised || conn == null) {
+            throw new RuntimeException("Trying to commit non initialized DataTableWorker!");
+        }
+        if (isDirty) {
+            if (unsavedRecords > 0) {
+//                log.info("Save next " + unsavedRecords + " records.");
+                int statemntsCount = 0;
+                try {
+                    executeCount++;
+                    statemntsCount = stmt.executeBatch().length;
+                    stmt.clearWarnings();
+                    sqls.clear();
+                    stmt.clearBatch();
+                    conn.commit();
+                    isDirty = false;
+                    unsavedRecords = 0;
+//                    stmt = conn.createStatement();
+                   } catch (BatchUpdateException e) {
+                    totalErrorsCount++;
+                    String wrongSql = "";
+                    boolean hasExecuteFailed = false;
+                    for (int i = 0; i < e.getUpdateCounts().length; i++) {
+                        int state = e.getUpdateCounts()[i];
+                        if(state == Statement.EXECUTE_FAILED) {
+                            hasExecuteFailed = true;
+                            wrongSql+= "SQL: \"" + sqls.remove(0)+"\"" ;
+                        } else {
+                            sqls.remove(0);
+                        }
+                    }
+                    if(!hasExecuteFailed) {
+                        wrongSql+= "SQL: \"" + sqls.remove(0)+"\"";
+                    }
+                    statemntsCount = unsavedRecords - sqls.size() - 1;
+                    log.error("Found error \"" + e.getMessage() + "\n" + wrongSql);
+//                    sqls = sqls.subList(sucessCount + 1, sqls.size());
+                    stmt.clearWarnings();
+                    conn.clearWarnings();
+                    conn.commit();
+                    stmt.clearBatch();
+//                    stmt = conn.createStatement();
+                    for (String sql : sqls) {
+                        stmt.addBatch(sql);
+                    }
+                    unsavedRecords = sqls.size();
+                    isDirty = unsavedRecords>0;
+                } finally {
+                    totalCommited+= statemntsCount;
+                    log.info("Batch records are saved: " + statemntsCount + "\tIn totoal: "  + totalCommited + "\tErrors: " + totalErrorsCount);
+                    conn.clearWarnings();
+                    conn.commit();
+                }
+            }
+        }
+    }
+
+    public void close() {
+    worker = null;
+    initialised = false;
+    if (isClosed) {
+        return;
+    }
+    log.info("Total commited statements: " + totalCommited);
+    log.info("Total errors count: " + totalErrorsCount);
+    isClosed = true;
+    if (isDirty) {
+        log.error("Found dirty data base session state!! Will be rollback.");
+        try {
+            conn.rollback();
+        } catch (Exception e) {
+            log.error("Error rollback dirty transaction!", e);
+        } finally {
+            isDirty = false;
+        }
+    }
+    try {
+        try {
+            if (conn != null)
+                conn.commit();
+        } finally {
+            if (conn != null && !conn.isClosed())
+                conn.close();
+        }
+    } catch (Exception e) {
+        log.error("Found exception when try to close DB resources!", e);
+        throw new RuntimeException(e);
+    }
+
+}
+
+}
---
+++ trunk/Excel2DB/src/ru/btc/utils/excel2csv/Main2.java
@@ -0,0 +1,99 @@
+package ru.btc.utils.excel2csv;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.eventusermodel.XLSXReader;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * User: Vova
+ * Date: 28.01.14
+ * Time: 0:48
+ */
+public class Main2 {
+
+    private static DataBaseWorker dataBaseWorker;
+
+
+    public static void main(String[] args) throws IOException, SQLException {
+        int argNum = 0;
+        String fileName = args[argNum++];
+        String sheetName = args[argNum++];
+        final String skipFistLine = args[argNum++];
+        final String dbTableName = args[argNum++];
+        final String dbFields = args[argNum++];
+        final String dbValuesFrom = args[argNum++];
+        String dbURI = args[argNum++];
+        String dbUser = args[argNum++];
+        String dbPass = args[argNum++];
+        final int rowsToProcess = Integer.parseInt(args[argNum++]);
+        System.out.println("Параметры запуска:");
+        for (String arg : args) {
+            System.out.print("\"" + arg + "\" ");
+        }
+
+        dataBaseWorker = new DataBaseWorker(dbURI, dbUser, dbPass, 1000);
+
+        try {
+
+            Date started = new Date();
+            System.out.println("\nОткрытие файла: \"" + fileName + "\"..");
+
+
+            XLSXReader.main(fileName, sheetName, new XLSXReader.RowHandlller() {
+                private int lineNum = 0;
+                Pattern pattern = Pattern.compile("#(\\w+)");
+
+                public void processRow(HashMap<String, String> rowData) {
+                    lineNum++;
+                    if(rowsToProcess > 0 && lineNum > rowsToProcess) {
+                        throw new RuntimeException("STOPPED");
+                    }
+                    if(lineNum==1 && (skipFistLine.equalsIgnoreCase("Y") || skipFistLine.equalsIgnoreCase("Yes"))) {
+                        return;
+                    }
+                    String sqlValues = dbValuesFrom;
+                    Matcher m = pattern.matcher(dbValuesFrom);
+                    while (m.find()) {
+                        String ref = m.group(1);
+                        String celllData = rowData.get(ref);
+                        if(celllData==null) {
+                            celllData = "";
+                        }
+                        celllData = celllData.replaceAll("'", "''");
+                        sqlValues = sqlValues.replaceFirst(m.group(), celllData);
+                    }
+
+                    String sql = "INSERT INTO " + dbTableName + "(" + dbFields + ") VALUES(" + sqlValues + ")";
+                    //System.out.println(sqlValues);
+                    try {
+                        dataBaseWorker.execSqlInBatch(sql);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+            dataBaseWorker.commitAllSkipErrors();
+            Date finished = new Date();
+            System.out.println("Started: " + started + "\nFinished: " + finished + "\nSeconds in processs: " + (finished.getTime()-started.getTime())/1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            dataBaseWorker.close();
+        }
+
+    }
+}
