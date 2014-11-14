05562fc813953d52feb549dce9c77ec6bc898830
ognivo777 <ognivo777@gmail.com>
2014-11-14 15:25:57 +0000

    minor chages

---
+++ Excel2DB/build_stub/sample_run_XLSX_import.cmd
@@ -0,0 +1,1 @@
+java -cp "sqlite-jdbc-3.8.7.jar;CsvExcelXLSX2DB.jar" ru.btc.utils.excel2csv.LoadXLSX SampleWorkBook.xlsx DataList Y USERS "NAME, DATE_OF_BIRTH" " '#C', date('#D')" "jdbc:sqlite:sample.db" dbuser dbpasswd 0 2000
